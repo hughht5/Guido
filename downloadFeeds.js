22be248725f34707e76c0fb2984056977de67aff
Hugh Halford-Thompson <hughht5@gmail.com>
2012-09-12 13:29:12 +0100

    added git push

--- downloadFeeds.js
+++ downloadFeeds.js
@@ -127,7 +127,7 @@ function postToGlog(posts){
 	function complete(){
 		if (!waiting) {
 			console.log("hugh done!");
-			exec('git add *', function(error, stdout, stderr) { //; git commit -a -m "adding new article: '+posts[x].title+'"; git push
+			exec('git add *; git commit -a -m "adding new article: '+posts[x].title+'"; git push', function(error, stdout, stderr) {
 				if(error) {
 					console.log('Could not commit and push new articles: ' + error);
 				}
