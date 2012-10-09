80a7730ccc40c8031482a3b02aed8acfb564b398
Hugh Halford-Thompson <hughht5@gmail.com>
2012-10-09 10:05:39 +0000

    adding new article

--- downloadFeedsDirect.js
+++ downloadFeedsDirect.js
@@ -55,13 +55,8 @@ function downloadFeeds(first){
 
 //parse xml, convert it to JSON, and extract blog feeds.
 function parse(xml){
-	console.log(xml);
 	parser.parseString(xml, function (err, result) {
 		var posts = [];
-	//console.log(result);
-	//console.log(result.rss);
-	console.log(result.rss.channel[0]);	
-
 		//for each post, collect the values and add it to the posts array
 		for (var x=0;x<result.rss.channel[0].item.length;x++){
 			var post = new Object();
